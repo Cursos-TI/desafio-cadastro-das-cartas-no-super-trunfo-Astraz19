c54b1cdef5dcf790837826aa689ff09818d6c123
Astraz19 <num.astra19@gmail.com>
2025-04-10 13:29:52 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,96 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
+    //VARIAVEIS
+    char estado1, estado2;
+    char codigo_carta1 [10], codigo_carta2 [10];
+    char nome_cidade1 [30], nome_cidade2 [30];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int numero_pontos_turisticos1, numero_pontos_turisticos2;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    //Entrada de dados para a primeira carta
+
+    printf("Vamos cadastrar a primeira carta. \n");
+
+    // Sigla do estado da primeira carta
+    printf("Digite a sigla inicial do estado: ");
+    scanf(" %c" , &estado1);
+
+    // Código da  primeira carta
+    printf("Digite o codigo da carta, siga este formato: A001 (A= inicial do estado, 001= numero da carta): ");
+    scanf(" %s", codigo_carta1);
+
+    // Nome da cidade da primeira carta
+    printf("Digite o nome da cidade: ");
+    scanf(" %s", nome_cidade1);
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // População da primeira carta
+    printf("Digite a população da cidade: ");
+    scanf(" %d", &populacao1);
+
+    // Area da primeira carta
+    printf("Digite a area da cidade: ");
+    scanf(" %f", &area1);
+
+    // PIB da primeira carta
+    printf("Digite o pib da cidade: ");
+    scanf("%f", &pib1);
+
+    // Número de pontos turísticos da primeira carta
+    printf("Digite o numero de pontos turisticos da cidade: ");
+    scanf(" %d", &numero_pontos_turisticos1);
+
+   //Entrada de dados para a segunda carta
+    printf("Vamos cadastrar a segunda carta.\n");
+   printf("Digite a sigla inicial do estado: ");
+   scanf(" %c", &estado2);
+
+   printf("Digite o codigo da carta, siga este formato: A001 (A= inicial do estado, 001= numero da carta): ");
+   scanf(" %s", codigo_carta2);
+
+    printf("Digite o nome da cidade: ");
+    scanf(" %s", nome_cidade2);
+
+    printf("Digite a população da cidade: ");
+    scanf(" %d", &populacao2);
+
+    printf("Digite a area da cidade: ");
+    scanf(" %f", &area2);
+
+    printf("Digite o pib da cidade: ");
+    scanf("%f", &pib2);
+
+    printf("Digite o numero de pontos turisticos da cidade: ");
+    scanf(" %d", &numero_pontos_turisticos2);
+
+    // Exibindo os dados da primeira carta
+    printf("\n\nDados da primeira carta:\n");
+    printf("Sigla do estado: %c\n", estado1);
+    printf("Código da carta: %s\n", codigo_carta1);
+    printf("Nome da cidade: %s\n", nome_cidade1);
+    printf("População: %d km²\n", populacao1);
+    printf("Área: %.2f\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("Número de pontos turísticos: %d\n", numero_pontos_turisticos1);   
+
+    // Exibindo os dados da segunda carta
+    printf("\n\nDados da segunda carta:\n");
+    printf("Sigla do estado: %c\n", estado2);
+    printf("Código da carta: %s\n", codigo_carta2);
+    printf("Nome da cidade: %s\n", nome_cidade2);
+    printf("População: %d km²\n", populacao2);
+    printf("Área: %.2f\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("Número de pontos turísticos: %d\n", numero_pontos_turisticos2);
+
 
     return 0;
+
+
+
+
+
+
 }
